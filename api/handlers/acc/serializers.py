7595643f6c2bc8f336a2484d1ad7ad4445d198c8
Alexey Baranov <atlantij@gmail.com>
2022-02-03 12:48:07 +0300

    fix new user

--- api/handlers/acc/serializers.py
+++ api/handlers/acc/serializers.py
@@ -101,7 +101,7 @@ class UserManageSerializer(ModelSerializerWithCallCleanMethod):
                 role_instance = main_models.OrganizationProjectUserRole(**role_kwargs, role=row['role'])
                 role_instance.clean()
                 role_instance.save()
-        if is_new:
+        if not password and is_new:
             self.instance.generate_password_and_send_invite()
         return self.instance
 
