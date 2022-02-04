ce9a2bacd252d354b689e10414eddc1b2eec9010
Alexey Baranov <atlantij@gmail.com>
2022-02-04 13:49:43 +0300

    CMD cv_fix_positions_years

--- apps/cv/management/commands/cv_fix_positions_years.py
+++ apps/cv/management/commands/cv_fix_positions_years.py
@@ -3,10 +3,12 @@ import datetime
 from django.core.management.base import BaseCommand
 
 from cv import models as cv_models
+from project.contrib.db import get_sql_from_queryset
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for cv_pos in cv_models.CvPosition.objects.filter(year_started__lte=0):
-            cv_pos.year_started = datetime.datetime.now().year - cv_pos.year_started
+        qs = cv_models.CvPosition.objects.filter(year_started__gt=datetime.datetime.now().year)
+        for cv_pos in qs:
+            cv_pos.year_started = datetime.datetime.now().year + (datetime.datetime.now().year - cv_pos.year_started)
             cv_pos.save()
